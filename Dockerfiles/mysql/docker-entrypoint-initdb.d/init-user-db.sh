0b60256fcfab0b173137158c9b74669093d95962
中條航紀 <ck227200@gmail.com>
2020-05-02 00:46:41 +0900

    :+1:Change example to sample_app

--- Dockerfiles/mysql/docker-entrypoint-initdb.d/init-user-db.sh
+++ Dockerfiles/mysql/docker-entrypoint-initdb.d/init-user-db.sh
@@ -2,8 +2,8 @@
 set -e
 
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
-CREATE DATABASE example_development;
-CREATE DATABASE example_test;
+CREATE DATABASE sample_app_development;
+CREATE DATABASE sample_app_test;
 EOSQL
 
 mysql -uroot -p"$MYSQL_ROOT_PASSWORD" -Dexample_development < /docker-entrypoint-initdb.d/example.dmp
