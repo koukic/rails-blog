c7a53253d6d120950fe0cc92ad4d2be9fe53f329
中條航紀 <ck227200@gmail.com>
2020-07-29 23:37:42 +0900

    :+1:Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  scope module: 'authors' do
+    resources :posts
+  end
+  devise_for :authors
+  get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
+  # devise_for :authors, controllers: {
+  #   registrations: 'registrations'
+  # }
+  resource :session, only: %i(new create destroy)
 end
