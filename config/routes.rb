08afc6c3c7b1ef26ae6a691df6b29bfc17260988
中條航紀 <ck227200@gmail.com>
2020-07-30 16:45:31 +0900

    :sparkles:Change erb2slim

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,4 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
-  # devise_for :authors, controllers: {
-  #   registrations: 'registrations'
-  # }
-  resource :session, only: %i(new create destroy)
 end
