0e0bf2e3ff2b7324e315243b63ba47fbdfe83348
中條航紀 <ck227200@gmail.com>
2020-08-18 14:45:04 +0900

    :tada:Complete blog

--- app/javascript/controllers/elements_controller.js
+++ app/javascript/controllers/elements_controller.js
@@ -16,11 +16,15 @@
 // }
 
 import { Controller } from "stimulus";
+// import ApplicationController from "./application_controller";
 import Sortable from "sortablejs";
+import StimulusReflex from 'stimulus_reflex'
 
 export default class extends Controller {
+// export default class extends ApplicationController {
 
   connect() {
+    StimulusReflex.register(this)
     console.log('hello');
     let sortable_elements = document.getElementById("elements");
     Sortable.create(sortable_elements, {
@@ -29,6 +33,18 @@ export default class extends Controller {
   }
 
   end(event) {
-    console.log(event);
+    console.log(event.item);
+    let element = document.getElementById("elements");
+    console.log(element);
+    let element_items = Array.from(
+      document.getElementsByClassName("element-item")
+    );
+    console.log(element_items);
+    let elements = element_items.map((element, index) => {
+      return { id: element.dataset.id, position: index + 1 };
+    });
+    console.log(elements);
+    element.dataset.elements = JSON.stringify(elements);
+    this.stimulate("ElementsReflex#sort", element);
   }
 }
