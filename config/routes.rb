956ced4c4878bce526ca9ebd711561e258fae37c
中條航紀 <ck227200@gmail.com>
2020-09-05 11:28:58 +0900

    :+1:Change route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   end
   devise_for :authors
   get 'home/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   root to: "readers/home#index"
+  get '/blog/:id' => 'readers/posts#show', as: :blog_post
+
+
 end
