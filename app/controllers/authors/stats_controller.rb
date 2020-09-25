59e1ade4e512b02e1b11e3e96d7bb89118719418
中條航紀 <ck227200@gmail.com>
2020-09-25 13:16:58 +0900

    :bug:Change local time

--- app/controllers/authors/stats_controller.rb
+++ app/controllers/authors/stats_controller.rb
@@ -1,8 +1,8 @@
 module Authors
   class StatsController < AuthorsController
     def index
-      @count_by_date = PageView.for_date_range(7.days.ago, Date.today).count_by_date.to_json
-      @unique_count_by_date = PageView.for_date_range(7.days.ago, Date.today).uniq_count_by_date.to_json
+      @count_by_date = PageView.count_by_date(7.days.ago, Date.today).to_json
+      @unique_count_by_date = PageView.uniq_count_by_date(7.days.ago, Date.today).to_json
     end
   end
 end
--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -7,11 +7,20 @@ class PageView < Impression
     where(created_at: start_date.beginning_of_day..end_date.end_of_day)
   end
 
-  scope :count_by_date, -> do
-    group('date(created_at)').count
+  def self.count_by_date(start_date, end_date)
+    results = for_date_range(start_date, end_date).group('date(created_at)').count
   end
 
-  scope :uniq_count_by_date, -> do
-    select(:ip_address).distinct.group('date(created_at)').count
+  def self.uniq_count_by_date(start_date, end_date)
+    results = for_date_range(start_date, end_date).select(:ip_address).distinct.group('date(created_at)').count
+  end
+
+  def with_zeros(start_date, end_date, results_hash)
+    date_range = (start_date.to_date..end_date.to_date).to_a
+    hash_with_zeros = {}
+    date_range.each do |date|
+      hash_with_zeros[date] = results_hash[date] || 0
+    end
+    hash_with_zeros
   end
 end
