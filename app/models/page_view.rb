ce523741966b392716fa2911b1c16b4343ab5d8c
中條航紀 <ck227200@gmail.com>
2020-09-26 15:42:40 +0900

    :+1:Add pageview method

--- app/models/page_view.rb
+++ app/models/page_view.rb
@@ -9,14 +9,19 @@ class PageView < Impression
 
   def self.count_by_date(start_date, end_date)
     results = for_date_range(start_date, end_date).group('date(created_at)').count
+
+    with_zeros(start_date, end_date, results)
   end
 
   def self.uniq_count_by_date(start_date, end_date)
     results = for_date_range(start_date, end_date).select(:ip_address).distinct.group('date(created_at)').count
+    with_zeros(start_date, end_date, results)
   end
 
-  def with_zeros(start_date, end_date, results_hash)
-    date_range = (start_date.to_date..end_date.to_date).to_a
+  def self.with_zeros(start_date, end_date, results_hash)
+    date_range = (start_date.to_date..end_date.to_date).to_a.map do |date|
+      date
+    end
     hash_with_zeros = {}
     date_range.each do |date|
       hash_with_zeros[date] = results_hash[date] || 0
